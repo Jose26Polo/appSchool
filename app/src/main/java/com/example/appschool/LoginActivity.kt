61cd0d89bda67e8dc2b263e6f1ad2c702ab7bdf2
Jose Roberto Grego <jgrego63@gmail.com>
2023-03-28 21:39:55 -0300

    login and splash activity

---
+++ app/src/main/java/com/example/appschool/LoginActivity.kt
@@ -0,0 +1,72 @@
+package com.example.appschool
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.example.appschool.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+        window.statusBarColor = Color.parseColor("#FFFFFF")
+
+        binding.btnEntrar.setOnClickListener {
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when{
+                email.isEmpty() -> {
+                    binding.editEmail.error = "Preencha o E-mail!"
+                }
+                senha.isEmpty() -> {
+                    binding.editSenha.error = " Preencha a Senha!"
+                }
+                !email.contains("@gmail.com") -> {
+                    val snackbar = Snackbar.make(it,"E-mail invalido!", Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+                }
+                senha.length <= 5 -> {
+                    val snackbar = Snackbar.make(it,"A senha precisa ter pelo menos 6 caracteres!",
+                        Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+                }
+                else -> {
+                    login(it)
+
+                }
+            }
+        }
+
+    }
+    private fun login(view: View) {
+        val progressbar = binding.progressBar
+        progressbar.visibility = View.VISIBLE
+
+        binding.btnEntrar.isEnabled = false
+        binding.btnEntrar.setTextColor(Color.parseColor("#FFFFFF"))
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            navegarTelaPrincipal()
+            val snackbar = Snackbar.make(view,"A senha precisa ter pelo menos 6 caracteres!",
+                Snackbar.LENGTH_SHORT)
+            snackbar.show()
+        },3000)
+    }
+    private fun navegarTelaPrincipal(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ app/src/main/java/com/example/appschool/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.appschool
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        window.statusBarColor = Color.parseColor("#FFFFFF")
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
